e8ab6fda8f49b5a044fa48c8ca2bc1ff99871c03
Helena Rentschler <helrentschler@gmail.com>
2021-06-17 16:40:33 -0300

    end

--- pratica/un1a.c
+++ pratica/un1a.c
@@ -2,15 +2,23 @@
 Entrada: nome, idade, altura
 Processamento: 2021 - idade = ano de nascimento
 Saída: nome, idade, altura, ano de nascimento */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-
 int main(){
     char nome[10];
-    unsigned float
-    printf("Digite o seu nome: ")
-    scanf("",)
-    
+    short int idade, ano;
+    float altura;
+    printf("Digite o seu nome: \n");
+    scanf("%s", nome);
+    printf("Digite a sua idade: \n");
+    scanf("%d", &idade);
+    printf("Digite a sua altura em metros: \n");
+    scanf("%e", &altura);
+    ano = 2021 - idade;
+    printf("Seu nome é: %s \n", nome);
+    printf("Você tem %d anos\n", idade);
+    printf("Nasceu em %d \n", ano);
+    printf("E tem altura de %em \n", altura);
+    return(0);
 }
