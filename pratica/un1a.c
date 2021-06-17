bb4ba0917ed18fe6a2654ec7725eb2db1530ddf3
Helena Rentschler <helrentschler@gmail.com>
2021-06-17 15:19:40 -0300

    exercises file

---
+++ pratica/un1a.c
@@ -0,0 +1,16 @@
+/* un 1 - construindo um programa 
+Entrada: nome, idade, altura
+Processamento: 2021 - idade = ano de nascimento
+Saída: nome, idade, altura, ano de nascimento */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+    char nome[10];
+    unsigned float
+    printf("Digite o seu nome: ")
+    scanf("",)
+    
+}
