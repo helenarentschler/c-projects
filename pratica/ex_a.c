a286c5cd6d4a8cb456aecfc0ca2d715f034fb3a2
Helena Rentschler <helrentschler@gmail.com>
2021-06-28 16:46:05 -0300

    ex a

--- pratica/ex_a.c
+++ pratica/ex_a.c
@@ -1,24 +1,24 @@
 /* un 1 - construindo um programa 
 Entrada: nome, idade, altura
 Processamento: 2021 - idade = ano de nascimento
-Saída: nome, idade, altura, ano de nascimento */
+Saida: nome, idade, altura, ano de nascimento */
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 int main(){
     char nome[10];
-    short int idade, ano;
+    int idade, ano;
     float altura;
     printf("Digite o seu nome: \n");
     scanf("%s", nome);
     printf("Digite a sua idade: \n");
     scanf("%d", &idade);
     printf("Digite a sua altura em metros: \n");
-    scanf("%e", &altura);
+    scanf("%f", &altura);
     ano = 2021 - idade;
-    printf("Seu nome é: %s \n", nome);
-    printf("Você tem %d anos\n", idade);
+    printf("Seu nome eh: %s \n", nome);
+    printf("Voce tem %d anos\n", idade);
     printf("Nasceu em %d \n", ano);
-    printf("E tem altura de %em \n", altura);
+    printf("E tem altura de %f \n", altura);
     return(0);
 }
