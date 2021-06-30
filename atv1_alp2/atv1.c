098680164dfdc74a20a04eaf0d8d55b3b97ebce3
Helena Rentschler <helrentschler@gmail.com>
2021-06-30 17:04:02 -0300

    atv1 update

--- atv1_alp2/atv1.c
+++ atv1_alp2/atv1.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 int main (){
-	int 
+	float s;
+	printf("Helena Rentschler - RA: 21141259-5 \n");
+	printf("RenTech - Reajuste de Salario \n");
+	printf("Insira seu salario liquido: ");
+	scanf ("%f", &s);
+	if (s > 1750.00){
+		s = s*1.075;
+		printf("Salario reajustado: %.2f", s);
+	}
+	else {
+		s = s*1.075 + 150.00;
+		printf("Salario reajustado: %.2f", s);
+	}
+	return(0);
 }
--- pratica/ex_a.c
+++ pratica/ex_a.c
@@ -15,7 +15,7 @@ int main(){
     printf("Digite a sua altura em metros: \n");
     scanf("%f", &altura);
     ano = 2021 - idade;
-    printf("Seu nome é: %s \n", nome);
+    printf("Seu nome eh: %s \n", nome);
     printf("Voce tem %d anos\n", idade);
     printf("Nasceu em %d \n", ano);
     printf("E tem altura de %.2f \n", altura);
