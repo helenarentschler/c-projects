6ea6e804a55fb2a1c032c4b47a795022c17a455a
Helena Rentschler <helrentschler@gmail.com>
2021-07-02 19:40:38 -0300

    Update atv1.c

--- atv1_alp2/atv1.c
+++ atv1_alp2/atv1.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main (){
-	float s;
-	printf("Helena Rentschler - RA: 21141259-5 \n");
-	printf("RenTech - Reajuste de Salario \n");
-	printf("Insira seu salario liquido: ");
-	scanf ("%f", &s);
-	if (s > 1750.00){
-		s = s*1.075;
-		printf("Salario reajustado: %.2f", s);
-	}
-	else {
-		s = s*1.075 + 150.00;
-		printf("Salario reajustado: %.2f", s);
+int main(){
+	float sliq, sreaj;
+	char op;
+	printf("Helena Rentschler - RA: 21141259-5 \n\n");
+	printf("RenTech - Reajuste de Salario \n\n");
+	op = 's';
+	while (op == 's'){
+		printf("Insira seu salario liquido: ");
+		scanf("%f", &sliq);
+		if (sliq > 1750.00){
+			sreaj = sliq * 1.075;
+		}
+		else{
+			sreaj = sliq * 1.075 + 150.00;
+		}
+		printf("\nSalario liquido informado: R$ %.2f \n", sliq); 
+		printf("Salario reajustado: R$ %.2f \n", sreaj);
+		printf("Digite 's' para calcular novamente e 'n' para sair: ");
+		scanf("%s", &op);
+		system("cls");
 	}
 	return(0);
 }
