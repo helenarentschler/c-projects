a405dd4ccecc054765fd9b6724cbc6ea7cd73360
Helena Rentschler <helrentschler@gmail.com>
2021-07-05 18:52:36 -0300

    update

---
+++ practice/calculator1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	double num1, num2;
+	printf("Enter the first number: ");
+	scanf("%lf", &num1);
+	printf("Enter the second number: ");
+	scanf("%lf", &num2);
+	printf("Answer: %f", num1 + num2);
+	return 0;
+}
--- practice/input.c
+++ practice/input.c
@@ -5,15 +5,17 @@ int main(){
 	int age;
 	double gpa;
 	char grade, name[20];
-	printf("Enter your name: ")
+	printf("Enter your name: ");
+	fgets(name, 20, stdin); /* scanf only works up till the first space, hence the error. Use fgets with strings. standart input = console */
 	printf("Enter your age: ");
 	scanf("%d", &age);
 	printf("Enter your gpa: ");
 	scanf("%lf", &gpa);
-	printf("Enter your grade: ");
-	scanf("%c", &grade);
+	/*printf("Enter your grade: ");
+	scanf("%c", &grade);*/
+	printf("Your name is: %s\n", name); /* fgets - new line printed after name */
 	printf("You are %d years old\n", age);
 	printf("Your gpa is: %f\n", gpa);
-	printf("Your grade is: %c", grade);
+	/*printf("Your grade is: %c", grade);*/
 	return 0;
 }
