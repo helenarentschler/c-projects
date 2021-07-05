b617f5ff7718e2abb1a661fa724e8aa75b1390cf
Helena Rentschler <helrentschler@gmail.com>
2021-07-05 16:26:40 -0300

    update2

--- practice/input.c
+++ practice/input.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
-#include <>
+#include <stdlib.h>
+
+int main(){
+	int age;
+	double gpa;
+	char grade, name[20];
+	printf("Enter your name: ")
+	printf("Enter your age: ");
+	scanf("%d", &age);
+	printf("Enter your gpa: ");
+	scanf("%lf", &gpa);
+	printf("Enter your grade: ");
+	scanf("%c", &grade);
+	printf("You are %d years old\n", age);
+	printf("Your gpa is: %f\n", gpa);
+	printf("Your grade is: %c", grade);
+	return 0;
+}
