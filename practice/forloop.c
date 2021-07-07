329ded6102fe1d56768e8d46d967907d9e23ffe4
Helena Rentschler <helrentschler@gmail.com>
2021-07-07 20:34:31 -0300

    Create forloop.c

---
+++ practice/forloop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int i;
+	int luckyNumbers[] = {1,7,8,9,19,38};
+	for (i = 0; i < 6; i++){
+		printf("%d\n", luckyNumbers[i]);
+	}
+	return 0;
+}
